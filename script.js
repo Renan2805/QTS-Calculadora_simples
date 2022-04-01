9dafb4ded37c6f2942d250e6efc989292da16926
Renan <renansilva.dev@gmail.com>
2022-03-31 22:23:51 -0300

    Comentarios adicionados a scriupt.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const form = document.querySelector('#form')
 const button = document.querySelector('#calc-btn')
 
 const calculate = (e) => {
+  // Previne que o botão de submit recarregue a pagina
 	e.preventDefault()
 	const number1 = form.n1.value
 	const number2 = form.n2.value
 	const operator = form.op.value
-
-	console.log(form.op.value)
 	
 	let result = 0 
-
+  
+  // Verifica o operador e faz o calculo
 	switch(operator) {
 		case '+':
 			result = parseInt(number1) + parseInt(number2)
@@ -32,13 +32,14 @@ const calculate = (e) => {
 			alert('Insira um operador válido')
 			break
 	}
-
+  
+  // Reseta o formulario e foca no primeiro input
   form.reset()
   form.n1.focus()
 }
 
+// Não permite que a letra 'e' nem o '_' sejam inseridos no campo do operador
 form.op.addEventListener('keydown', (e) => {
-  console.log(e)
   let invalidChars= [
     'e',
     '_'
